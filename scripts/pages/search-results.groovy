e55249dc103bd68a2455b7af49be90b30e111688
admin admin <evaladmin@example.com>
2020-12-11 02:23:41 +0000

    User admin wrote content /scripts/pages/search-results.groovy

--- scripts/pages/search-results.groovy
+++ scripts/pages/search-results.groovy
@@ -13,8 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+println categories_o
 
 def categoriesItem = siteItemService.getSiteItem(contentModel.categories_o.item.key.text)
-println categories_o
 println categoriesItem
 templateModel.categories = categoriesItem.items.item
