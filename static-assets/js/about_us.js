2c9ceb85485d35bbdf10263c099554d9c3affb18
admin admin <evaladmin@example.com>
2020-12-11 07:14:07 +0000

    User admin wrote content /static-assets/js/about_us.js

---
+++ static-assets/js/about_us.js
@@ -0,0 +1,53 @@
+(function($) {
+
+   $(function() {
+     var queryParam = $.urlParam('q');
+     if (queryParam) {
+       queryParam = decodeURI(queryParam).trim();
+       $('#query').val(queryParam);
+     }
+
+     var source = $("#search-results-template").html();
+     var template = Handlebars.compile(source);
+
+     var doSearch = function(userTerm, years) {
+       var params = {};
+
+       if (userTerm) {
+         params.userTerm = userTerm;
+       }
+       
+       console.log(years)
+       
+       if (years) {
+         params.years = years;
+       }
+        console.log(params)
+       $.get("/api/searchByYears.json", params).done(function(data) {
+         if (data == null) {
+           data = [];
+         }
+        console.log(data)
+         var context = { results: data };
+         console.log(context)
+         var html = template(context);
+        //  console.log(html)
+
+         $('#search-results').html(html);
+       });
+     }
+
+     $('#years input').click(function() {
+       var years = [];
+
+       $('#years input:checked').each(function() {
+         years.push($(this).val());
+       });
+
+       doSearch(queryParam, years);
+     });
+
+     doSearch(queryParam);
+   });
+
+})(jQuery);
