b0f8dcf34eb0f088b34b00205d6a98bd3cd4cf66
admin admin <evaladmin@example.com>
2020-12-15 08:03:26 +0000

    User admin wrote content /scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy

--- scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
@@ -206,16 +206,16 @@ class SearchHelper {
     //added by alanlee 11-12-2020
     // def about_us_search(userTerm, categories, start = DEFAULT_START, rows = DEFAULT_ROWS){
     def about_us_search(userTerm, years, start = DEFAULT_START, rows = DEFAULT_ROWS){
-        // def q = "${ARTICLE_CONTENT_TYPE_QUERY}"
-        def q = "${NEWS_N_MEDIA_TYPE_QUERY}"
-        // if (userTerm) {
-        //   if(!userTerm.contains(" ")) {
-        //     userTerm = "${userTerm}~1 OR *${userTerm}*"
-        //   }
-        //   def userTermQuery = "(subject_t:(${userTerm}) OR sections_o.item.section_html:(${userTerm}))"
+        def q = "${ARTICLE_CONTENT_TYPE_QUERY}"
+        // def q = "${NEWS_N_MEDIA_TYPE_QUERY}"
+        if (userTerm) {
+          if(!userTerm.contains(" ")) {
+            userTerm = "${userTerm}~1 OR *${userTerm}*"
+          }
+          def userTermQuery = "(subject_t:(${userTerm}) OR sections_o.item.section_html:(${userTerm}))"
         
-        //   q = "${q} AND ${userTermQuery}"
-        // }
+          q = "${q} AND ${userTermQuery}"
+        }
         // if (categories) {
         //   def categoriesQuery = getFieldQueryWithMultipleValues("categories_o.item.key", categories)
         
