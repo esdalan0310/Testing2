e6f44aa26a9e02881f3892bebe9a4e79ee6bdc53
admin admin <evaladmin@example.com>
2020-12-15 07:48:48 +0000

    User admin wrote content /scripts/pages/about_us.groovy

--- scripts/pages/about_us.groovy
+++ scripts/pages/about_us.groovy
@@ -1,14 +1,2 @@
-def yearsItem = siteItemService.getSiteItem(contentModel.years_o.item.key.text)
-templateModel.yearss = yearsItem.items.item
-
-
-// import org.craftercms.sites.editorial.SearchHelper
-// import org.craftercms.sites.editorial.ProfileUtils
-
-// def segment = ProfileUtils.getSegment(profile, siteItemService)
-// def years = contentModel.years_o
-// def maxArticles = contentModel.max_articles_i
-// def searchHelper = new SearchHelper(elasticsearch, urlTransformationService)
-// def articles = searchHelper.searchArticles(false, years, segment, 0, maxArticles)
-
-// templateModel.yearss = articles
+def categoriesItem = siteItemService.getSiteItem(contentModel.years_o.item.key.text)
+templateModel.categories = categoriesItem.items.item
