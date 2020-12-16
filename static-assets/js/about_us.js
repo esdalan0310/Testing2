0a4db6847b3faf5ddb5d48e45677163a826d1978
admin admin <evaladmin@example.com>
2020-12-16 09:05:05 +0000

    User admin wrote content /static-assets/js/about_us.js

--- static-assets/js/about_us.js
+++ static-assets/js/about_us.js
@@ -1,79 +1,54 @@
-// (function($) {
+(function($) {
 
-//   $(function() {
-//      var queryParam = $.urlParam('q');
-//      if (queryParam) {
-//       queryParam = decodeURI(queryParam).trim();
-//       $('#query').val(queryParam);
-//      }
+  $(function() {
+     var queryParam = $.urlParam('q');
+     if (queryParam) {
+      queryParam = decodeURI(queryParam).trim();
+      $('#query').val(queryParam);
+     }
 
-//      var source = $("#search-results-template").html();
-//      var template = Handlebars.compile(source);
-     
-     
-//      var query = `query q {
-//                   page_article {
-//                     items {
-//                       author_s
-//                       date_dt
-//                       disabled
-//                       featured_b
-//                       file__name
-//                       image_s
-//                       internal__name
-//                       localId
-//                       meta_keywords_t
-//                       navLabel
-//                       orderDefault_f
-//                       placeInNav
-//                       subject_t
-//                       summary_t
-//                     }
-//                   }
-//                 }
-//                 `
+     var source = $("#search-results-template").html();
+     var template = Handlebars.compile(source);
+     var doSearch = function(userTerm, years) {
+      var params = {};
 
-
-//      var doSearch = function(userTerm, years) {
-//       var params = {};
-
-//       if (userTerm) {
-//          params.userTerm = userTerm;
-//       }
+      if (userTerm) {
+         params.userTerm = userTerm;
+      }
        
-//       console.log(years)
+      console.log(years)
        
-//       if (years) {
-//          params.years = years;
-//       }
-//         console.log(params)
-//       $.get("/api/about_us.json", params).done(function(data) {
-//          if (data == null) {
-//           data = [];
-//          }
-//         console.log(data)
-//          var context = { results: data };
-//          console.log(context)
-//          var html = template(context);
+      if (years) {
+         params.years = years;
+      }
+        console.log(params)
+      $.get("/api/about_us.json", params).done(function(data) {
+         if (data == null) {
+          data = [];
+         }
+        console.log(data)
+         var context = { results: data };
+         console.log(context)
+         var html = template(context);
 
-//          $('#search-results').html(html);
-//       });
-//      }
+         $('#search-results').html(html);
+      });
+     }
 
-//      $('#years input').click(function() {
-//       var years = [];
+     $('#years input').click(function() {
+      var years = [];
 
-//       $('#years input:checked').each(function() {
-//          years.push($(this).val());
-//       });
+      $('#years input:checked').each(function() {
+         years.push($(this).val());
+      });
 
-//       doSearch(queryParam, years);
-//      });
+      doSearch(queryParam, years);
+     });
 
-//      doSearch(queryParam);
-//   });
+     doSearch(queryParam);
+  });
 
-// })(jQuery);
+})(jQuery);
 //=================
 
 $(document).ready(function() {
