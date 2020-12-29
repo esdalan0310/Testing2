0d880163de5de6a90805684752c61c778934016f
admin admin <evaladmin@example.com>
2020-12-29 04:11:44 +0000

    User admin wrote content /scripts/pages/aboutus.groovy

--- scripts/pages/aboutus.groovy
+++ scripts/pages/aboutus.groovy
@@ -6,5 +6,4 @@ Date date = new Date()
 String datePart = date.format("dd/MM/yyyy")
 String timePart = date.format("HH:mm:ss")
 println "datePart : " + datePart + "\ttimePart : " + timePart
-templateModel.years = "datePart : " + datePart + "\ttimePart : " + timePart
 // templateModel.test123 = "alanalan123123213212321"
