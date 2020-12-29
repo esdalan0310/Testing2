60f9b15970240deb28afc3ce2d1b875f659e6e6e
admin admin <evaladmin@example.com>
2020-12-29 03:02:06 +0000

    User admin wrote content /static-assets/js/about_us.js

--- static-assets/js/about_us.js
+++ static-assets/js/about_us.js
@@ -63,7 +63,7 @@ function resetQuery(){
      
 var doGraphql = function (yearFilterQuery) {
    $("#testDiv").html('loading....');
-   $.ajax({url: "http://18.163.110.47:8080/api/1/site/graphql",
+   $.ajax({url: "http://18.163.110.47:8080/api/1/site/graphql?crafterSite=demo",
       contentType: "application/json",
       type:'POST',
       headers: { 'Cookie': 'crafterSite=demo' },
