1746fed61168ba5afd1375fb7b5d9ef25b1f0f11
admin admin <evaladmin@example.com>
2020-12-29 04:10:23 +0000

    User admin wrote content /scripts/pages/aboutus.groovy

--- scripts/pages/aboutus.groovy
+++ scripts/pages/aboutus.groovy
@@ -2,4 +2,8 @@ def yearsItem = siteItemService.getSiteItem(contentModel.years_o.item.key.text)
 // println yearsItem
 templateModel.years = yearsItem.items.item
 println "groovy gen"
+Date date = new Date()
+String datePart = date.format("dd/MM/yyyy")
+String timePart = date.format("HH:mm:ss")
+println "datePart : " + datePart + "\ttimePart : " + timePart
 templateModel.test123 = "alanalan123123213212321"
