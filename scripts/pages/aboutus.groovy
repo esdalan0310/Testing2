7ce7bc8cdaec98ff899fd9d053214843020fdba2
admin admin <evaladmin@example.com>
2020-12-29 04:09:16 +0000

    User admin wrote content /scripts/pages/aboutus.groovy

--- scripts/pages/aboutus.groovy
+++ scripts/pages/aboutus.groovy
@@ -1,4 +1,5 @@
 def yearsItem = siteItemService.getSiteItem(contentModel.years_o.item.key.text)
 // println yearsItem
 templateModel.years = yearsItem.items.item
+println "groovy gen"
 templateModel.test123 = "alanalan123123213212321"
