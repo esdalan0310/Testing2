6b52f3befa0abf08dd8b3a43745ef67803394f34
admin admin <evaladmin@example.com>
2020-12-30 08:08:13 +0000

    User admin wrote content /scripts/pages/aboutus.groovy

--- scripts/pages/aboutus.groovy
+++ scripts/pages/aboutus.groovy
@@ -20,7 +20,7 @@ if(getRC.equals(200)) {
 def post = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
 def message = "{'username' : 'superuser1@esdlife.com','password': 'Ab123456!','type':'A'}";
 post.setRequestMethod("POST");
-// post.setDoOutput(true)
+post.setDoOutput(true);
 post.setRequestProperty("Content-Type", "application/json");
 post.getOutputStream().write(message.getBytes("UTF-8"));
 def postRC = post.getResponseCode();
