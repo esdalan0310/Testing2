1a281a437d653e993c167cad4095ea459f5ae633
admin admin <evaladmin@example.com>
2020-12-31 02:38:30 +0000

    User admin wrote content /scripts/rest/about_us.get.groovy

--- scripts/rest/about_us.get.groovy
+++ scripts/rest/about_us.get.groovy
@@ -13,27 +13,27 @@
 
 
 
-// GET
-// def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
-def get = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
-def getRC = get.getResponseCode();
-println(getRC);
-if(getRC.equals(200)) {
-    // println(get.getInputStream().getText());
-    return get.getInputStream().getText();
-}else{
-    return 'no data'
-}
-
-// // POST
-// def post = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
-// def message = "";
-// post.setRequestMethod("POST");
-// post.setDoOutput(true);
-// post.setRequestProperty("Content-Type", "application/json");
-// post.getOutputStream().write(message.getBytes("UTF-8"));
-// def postRC = post.getResponseCode();
-// println(postRC);
-// if(postRC.equals(200)) {
-//     println(post.getInputStream().getText());
+// // GET
+// // def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
+// def get = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
+// def getRC = get.getResponseCode();
+// println(getRC);
+// if(getRC.equals(200)) {
+//     // println(get.getInputStream().getText());
+//     return get.getInputStream().getText();
+// }else{
+//     return 'no data'
 // }
+
+// POST
+def post = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
+def message = "{'username':'superuser1@esdlife.com','password':'Ab123456!','type':'A'}";
+post.setRequestMethod("POST");
+post.setDoOutput(true);
+post.setRequestProperty("Content-Type", "application/json");
+post.getOutputStream().write(message.getBytes("UTF-8"));
+def postRC = post.getResponseCode();
+println(postRC);
+if(postRC.equals(200)) {
+    println(post.getInputStream().getText());
+}
