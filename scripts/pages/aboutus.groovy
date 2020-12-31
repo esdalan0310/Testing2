3949f4c9cd0d3c95d3adb6a2d06555e7d9af2c06
admin admin <evaladmin@example.com>
2020-12-31 02:19:36 +0000

    User admin wrote content /scripts/pages/aboutus.groovy

--- scripts/pages/aboutus.groovy
+++ scripts/pages/aboutus.groovy
@@ -9,22 +9,23 @@ println "datePart : " + datePart + "\ttimePart : " + timePart
 // templateModel.test123 = "alanalan123123213212321"
 
 // GET
-def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
+// def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
+def get = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
 def getRC = get.getResponseCode();
 println(getRC);
 if(getRC.equals(200)) {
     println(get.getInputStream().getText());
 }
 
-// POST
-def post = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
-def message = "";
-post.setRequestMethod("POST");
-post.setDoOutput(true);
-post.setRequestProperty("Content-Type", "application/json");
-post.getOutputStream().write(message.getBytes("UTF-8"));
-def postRC = post.getResponseCode();
-println(postRC);
-if(postRC.equals(200)) {
-    println(post.getInputStream().getText());
-}
+// // POST
+// def post = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
+// def message = "";
+// post.setRequestMethod("POST");
+// post.setDoOutput(true);
+// post.setRequestProperty("Content-Type", "application/json");
+// post.getOutputStream().write(message.getBytes("UTF-8"));
+// def postRC = post.getResponseCode();
+// println(postRC);
+// if(postRC.equals(200)) {
+//     println(post.getInputStream().getText());
+// }
