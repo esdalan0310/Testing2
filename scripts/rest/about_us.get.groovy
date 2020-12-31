8dfa842577006a38e47b6e1bdc35c0693e10c38f
admin admin <evaladmin@example.com>
2020-12-31 02:28:04 +0000

    User admin wrote content /scripts/rest/about_us.get.groovy

--- scripts/rest/about_us.get.groovy
+++ scripts/rest/about_us.get.groovy
@@ -1,12 +1,39 @@
-import org.craftercms.sites.editorial.SearchHelper
+// import org.craftercms.sites.editorial.SearchHelper
 
-def userTerm = params.userTerm
+// def userTerm = params.userTerm
 
-def years = params["years[]"]
-// def categories = params["categories[]"]
-def start = params.start ? params.start as Integer : 0
-def rows = params.rows ? params.rows as Integer : 10
-def searchHelper = new SearchHelper(elasticsearch, urlTransformationService)
-def results = searchHelper.about_us_search(userTerm, years, start, rows)
+// def years = params["years[]"]
+// // def categories = params["categories[]"]
+// def start = params.start ? params.start as Integer : 0
+// def rows = params.rows ? params.rows as Integer : 10
+// def searchHelper = new SearchHelper(elasticsearch, urlTransformationService)
+// def results = searchHelper.about_us_search(userTerm, years, start, rows)
 
+// return results;
+
+
+
+// GET
+def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
+// def get = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
+def getRC = get.getResponseCode();
+def results = null;
+println(getRC);
+if(getRC.equals(200)) {
+    println(get.getInputStream().getText());
+    results = get.getInputStream().getText();
+}
 return results;
+
+// // POST
+// def post = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
+// def message = "";
+// post.setRequestMethod("POST");
+// post.setDoOutput(true);
+// post.setRequestProperty("Content-Type", "application/json");
+// post.getOutputStream().write(message.getBytes("UTF-8"));
+// def postRC = post.getResponseCode();
+// println(postRC);
+// if(postRC.equals(200)) {
+//     println(post.getInputStream().getText());
+// }
