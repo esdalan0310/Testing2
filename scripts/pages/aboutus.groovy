7fda2c47044a57921f87d10f752ef7cd63f91fc7
admin admin <evaladmin@example.com>
2021-01-04 02:54:55 +0000

    User admin wrote content /scripts/pages/aboutus.groovy

--- scripts/pages/aboutus.groovy
+++ scripts/pages/aboutus.groovy
@@ -8,14 +8,14 @@ String timePart = date.format("HH:mm:ss")
 println "datePart : " + datePart + "\ttimePart : " + timePart
 // templateModel.test123 = "alanalan123123213212321"
 
-// GET
-def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
-// def get = new URL("http://172.61.41.119/api/v1/admin/login").openConnection();
-def getRC = get.getResponseCode();
-println(getRC);
-if(getRC.equals(200)) {
-    println(get.getInputStream().getText());
-}
+// // GET
+// def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
+// // def get = new URL("http://172.61.41.119/api/v1/admin/login").openConnection();
+// def getRC = get.getResponseCode();
+// println(getRC);
+// if(getRC.equals(200)) {
+//     println(get.getInputStream().getText());
+// }
 
 // // POST
 // def post = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
