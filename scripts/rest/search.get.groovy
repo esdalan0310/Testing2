819c0a6ab40a0bc713d4b6f2323918f3f01f2817
admin admin <evaladmin@example.com>
2021-01-06 06:54:33 +0000

    User admin wrote content /scripts/rest/search.get.groovy

--- scripts/rest/search.get.groovy
+++ scripts/rest/search.get.groovy
@@ -21,6 +21,8 @@ def categories = params["categories[]"]
 def start = params.start ? params.start as Integer : 0
 def rows = params.rows ? params.rows as Integer : 10
 def searchHelper = new SearchHelper(elasticsearch, urlTransformationService)
-def results = searchHelper.search(userTerm, categories, start, rows)
+// def results = searchHelper.search(userTerm, categories, start, rows)
+
+def results = searchHelper.about_us_search(userTerm, start, rows)
 
 return results;
