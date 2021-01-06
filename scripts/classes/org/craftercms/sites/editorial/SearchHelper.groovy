73941a2caa9b7888877c30f9ea7a89989ef93524
admin admin <evaladmin@example.com>
2021-01-06 09:30:50 +0000

    User admin wrote content /scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy

--- scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
@@ -244,8 +244,8 @@ class SearchHelper {
     // def about_us_search(userTerm, categories, start = DEFAULT_START, rows = DEFAULT_ROWS){
     def about_us_search(userTerm, start = DEFAULT_START, rows = DEFAULT_ROWS){
     // def about_us_search(userTerm, years, start = DEFAULT_START, rows = DEFAULT_ROWS){
-        // def q = "${ARTICLE_CONTENT_TYPE_QUERY}"
-        def q = "${NEWS_N_MEDIA_TYPE_QUERY}"
+        def q = "${ARTICLE_CONTENT_TYPE_QUERY}"
+        // def q = "${NEWS_N_MEDIA_TYPE_QUERY}"
         // q = "${q} OR ${ARTICLE_CONTENT_TYPE_QUERY}"
         
         if (userTerm) {
