8a641b9000d3fe50dab1f0d8e4e438d2a1d92515
admin admin <evaladmin@example.com>
2021-01-06 04:24:18 +0000

    User admin wrote content /scripts/pages/aboutus.groovy

--- scripts/pages/aboutus.groovy
+++ scripts/pages/aboutus.groovy
@@ -9,7 +9,7 @@ String timePart = date.format("HH:mm:ss")
 templateModel.test_123 = "datePart : " + datePart + "\ttimePart : " + timePart
 // templateModel.site
 
-GET
+// GET
 // def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
 def get = new URL("http://172.61.41.119/api/v1/admin/login").openConnection();
 def getRC = get.getResponseCode();
