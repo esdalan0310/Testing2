d6c61d1e1aa454b74899a464efd72ea063da8bbf
admin admin <evaladmin@example.com>
2021-01-06 09:05:37 +0000

    User admin wrote content /scripts/rest/about_us.get.groovy

--- scripts/rest/about_us.get.groovy
+++ scripts/rest/about_us.get.groovy
@@ -1,15 +1,15 @@
-import org.craftercms.sites.editorial.SearchHelper
+// import org.craftercms.sites.editorial.SearchHelper
 
-def userTerm = params.userTerm
+// def userTerm = params.userTerm
 
-def years = params["years[]"]
-// def categories = params["categories[]"]
-def start = params.start ? params.start as Integer : 0
-def rows = params.rows ? params.rows as Integer : 10
-def searchHelper = new SearchHelper(elasticsearch, urlTransformationService)
-def results = searchHelper.about_us_search(userTerm, years, start, rows)
+// def years = params["years[]"]
+// // def categories = params["categories[]"]
+// def start = params.start ? params.start as Integer : 0
+// def rows = params.rows ? params.rows as Integer : 10
+// def searchHelper = new SearchHelper(elasticsearch, urlTransformationService)
+// def results = searchHelper.about_us_search(userTerm, years, start, rows)
 
-return results;
+// return results;
 
 
 
