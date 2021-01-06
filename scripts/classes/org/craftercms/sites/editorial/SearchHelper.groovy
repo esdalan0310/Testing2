e1bd7f1c4133b1c2c1dd509cd8ab1657fa022a1c
admin admin <evaladmin@example.com>
2021-01-06 09:27:54 +0000

    User admin wrote content /scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy

--- scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
@@ -152,7 +152,6 @@ class SearchHelper {
     return articles
   }
   
-  
    private def processUserSearchResults222(result) {
     def articles = []
     println "[[RESULT]]" + result
@@ -247,7 +246,7 @@ class SearchHelper {
     // def about_us_search(userTerm, years, start = DEFAULT_START, rows = DEFAULT_ROWS){
         // def q = "${ARTICLE_CONTENT_TYPE_QUERY}"
         def q = "${NEWS_N_MEDIA_TYPE_QUERY}"
-        q = "${q} OR ${ARTICLE_CONTENT_TYPE_QUERY}"
+        // q = "${q} OR ${ARTICLE_CONTENT_TYPE_QUERY}"
         
         if (userTerm) {
           if(!userTerm.contains(" ")) {
