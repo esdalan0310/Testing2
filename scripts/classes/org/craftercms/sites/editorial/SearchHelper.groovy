1ab4375e940e61a56607063abc2f91d7fea72c31
admin admin <evaladmin@example.com>
2021-01-06 08:13:48 +0000

    User admin wrote content /scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy

--- scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
+++ scripts/classes/org/craftercms/sites/editorial/SearchHelper.groovy
@@ -247,13 +247,13 @@ class SearchHelper {
     // def about_us_search(userTerm, years, start = DEFAULT_START, rows = DEFAULT_ROWS){
         // def q = "${ARTICLE_CONTENT_TYPE_QUERY}"
         def q = "${NEWS_N_MEDIA_TYPE_QUERY}"
-        // q = "${q} OR ${ARTICLE_CONTENT_TYPE_QUERY}"
+        q = "${q} OR ${ARTICLE_CONTENT_TYPE_QUERY}"
         
         if (userTerm) {
           if(!userTerm.contains(" ")) {
             userTerm = "${userTerm}~1 OR *${userTerm}*"
           }
-          def userTermQuery = "(subject_s:(${userTerm}) OR subject_t:(${userTerm}) )"
+          def userTermQuery = "(title_t:(${userTerm}) )"
             // println userTermQuery
           q = "${q} AND ${userTermQuery}"
         }
