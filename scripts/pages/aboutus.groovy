9d69042d54e5c9e4d8fd6dcb5193310e42826f01
admin admin <evaladmin@example.com>
2021-01-06 04:24:02 +0000

    User admin wrote content /scripts/pages/aboutus.groovy

--- scripts/pages/aboutus.groovy
+++ scripts/pages/aboutus.groovy
@@ -9,27 +9,27 @@ String timePart = date.format("HH:mm:ss")
 templateModel.test_123 = "datePart : " + datePart + "\ttimePart : " + timePart
 // templateModel.site
 
-// GET
-// // def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
-// def get = new URL("http://172.61.41.119/api/v1/admin/login").openConnection();
-// def getRC = get.getResponseCode();
-// println(getRC);
-// if(getRC.equals(200)) {
-//     println(get.getInputStream().getText());
-// }
+GET
+// def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
+def get = new URL("http://172.61.41.119/api/v1/admin/login").openConnection();
+def getRC = get.getResponseCode();
+println(getRC);
+if(getRC.equals(200)) {
+    println(get.getInputStream().getText());
+}
 
 // POST
-def post = new URL("http://172.61.41.119/api/v1/admin/login").openConnection();
-def message = "{'username':'superuser1@esdlife.com','password':'Ab123456!','type': 'A'}";
-post.setRequestMethod("POST");
-post.setDoOutput(true);
-post.setRequestProperty("Content-Type", "application/json");
-post.getOutputStream().write(message.getBytes("UTF-8"));
-def postRC = post.getResponseCode();
-println(postRC);
-if(postRC.equals(200)) {
-    println(post.getInputStream().getText());
-}
+// def post = new URL("http://172.61.41.119/api/v1/admin/login").openConnection();
+// def message = "{'username':'superuser1@esdlife.com','password':'Ab123456!','type': 'A'}";
+// post.setRequestMethod("POST");
+// post.setDoOutput(true);
+// post.setRequestProperty("Content-Type", "application/json");
+// post.getOutputStream().write(message.getBytes("UTF-8"));
+// def postRC = post.getResponseCode();
+// println(postRC);
+// if(postRC.equals(200)) {
+//     println(post.getInputStream().getText());
+// }
 
 // def baseUrl = new URL('http://18.163.105.240/api/v1/admin/login')
 // def queryString = ''
