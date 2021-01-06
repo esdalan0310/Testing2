8b1d7ff34579d80bc5035cb45ca8dcc3d1f77493
admin admin <evaladmin@example.com>
2021-01-06 04:19:30 +0000

    User admin wrote content /scripts/pages/aboutus.groovy

--- scripts/pages/aboutus.groovy
+++ scripts/pages/aboutus.groovy
@@ -11,25 +11,25 @@ templateModel.test_123 = "datePart : " + datePart + "\ttimePart : " + timePart
 
 // GET
 // def get = new URL("http://worldclockapi.com/api/json/est/now").openConnection();
-def get = new URL("http://172.61.41.119/api/v1/admin/login").openConnection();
-def getRC = get.getResponseCode();
-println(getRC);
-if(getRC.equals(200)) {
-    println(get.getInputStream().getText());
-}
-
-// // POST
-// def post = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
-// def message = "";
-// post.setRequestMethod("POST");
-// post.setDoOutput(true);
-// post.setRequestProperty("Content-Type", "application/json");
-// post.getOutputStream().write(message.getBytes("UTF-8"));
-// def postRC = post.getResponseCode();
-// println(postRC);
-// if(postRC.equals(200)) {
-//     println(post.getInputStream().getText());
+// def get = new URL("http://172.61.41.119/api/v1/admin/login").openConnection();
+// def getRC = get.getResponseCode();
+// println(getRC);
+// if(getRC.equals(200)) {
+//     println(get.getInputStream().getText());
 // }
+
+// POST
+def post = new URL("http://18.163.105.240/api/v1/admin/login").openConnection();
+def message = "";
+post.setRequestMethod("POST");
+post.setDoOutput(true);
+post.setRequestProperty("Content-Type", "application/json");
+post.getOutputStream().write(message.getBytes("UTF-8"));
+def postRC = post.getResponseCode();
+println(postRC);
+if(postRC.equals(200)) {
+    println(post.getInputStream().getText());
+}
 
 // def baseUrl = new URL('http://18.163.105.240/api/v1/admin/login')
 // def queryString = ''
